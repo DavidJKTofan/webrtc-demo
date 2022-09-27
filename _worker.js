36f344b495f29fd437c127791b151fefaae83ca9
DavidJKTofan <dtofan@learning.eoi.es>
2022-09-27 23:06:07 +0200

    updated

--- _worker.js
+++
@@ -1,5 +0,0 @@
-export default {
-    fetch(req, env) {
-      return new Response('Variable 1: ' + env.WEBRTC_PLAYBACK_URL_FROM_YOUR_LIVE_INPUT + 'Variable 2:' + env.WEBRTC_URL_FROM_YOUR_LIVE_INPUT)
-    }
-}
---
+++ functions/envvars.js
@@ -0,0 +1,12 @@
+const handleRequest = createPagesFunctionHandler({
+  build,
+  getLoadContext({ env }) {
+    return env;
+  },
+});
+
+export let loader: LoaderFunction = async ({ context }) => {
+    // Use context.YOUR_ENVIRONMENT_VARIABLE to access your environment variable.
+    const WEBRTC_URL_FROM_YOUR_LIVE_INPUT = context.WEBRTC_URL_FROM_YOUR_LIVE_INPUT
+    const WEBRTC_PLAYBACK_URL_FROM_YOUR_LIVE_INPUT = context.WEBRTC_PLAYBACK_URL_FROM_YOUR_LIVE_INPUT
+  }
