f74a86805a114cd808589e264b9e9441044b1203
DavidJKTofan <dtofan@learning.eoi.es>
2022-09-27 23:10:14 +0200

    Update envvars.js

--- functions/envvars.js
+++ functions/envvars.js
@@ -7,6 +7,6 @@ const handleRequest = createPagesFunctionHandler({
 
 export let loader: LoaderFunction = async ({ context }) => {
     // Use context.YOUR_ENVIRONMENT_VARIABLE to access your environment variable.
-    const WEBRTC_URL_FROM_YOUR_LIVE_INPUT = context.WEBRTC_URL_FROM_YOUR_LIVE_INPUT
-    const WEBRTC_PLAYBACK_URL_FROM_YOUR_LIVE_INPUT = context.WEBRTC_PLAYBACK_URL_FROM_YOUR_LIVE_INPUT
+    const WEBRTC_URL_FROM_YOUR_LIVE_INPUT = context.WEBRTC_URL_FROM_YOUR_LIVE_INPUT;
+    const WEBRTC_PLAYBACK_URL_FROM_YOUR_LIVE_INPUT = context.WEBRTC_PLAYBACK_URL_FROM_YOUR_LIVE_INPUT;
   }
